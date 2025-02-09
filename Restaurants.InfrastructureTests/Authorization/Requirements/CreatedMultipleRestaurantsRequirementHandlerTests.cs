8aef73966637cdca30da51f58d126d3f616d9df8
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-09 22:39:06 +1100

    Test Project renaming

---
+++ Restaurants.InfrastructureTests/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Identity;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Infrastructure.Authorization.Requirements.Tests;
+
+public class CreatedMultipleRestaurantsRequirementHandlerTests
+{
+    [Fact()]
+    public async Task HandleRequirementAsync_UserHasNotCreatedMultipleRestaurants_ShouldFail()
+    {
+        // arrange
+
+        var currentUser = new CurrentUser("1", "test@test.com", [], null, null);
+        var userContextMock = new Mock<IUserContext>();
+        userContextMock.Setup(m => m.GetCurrentUser()).Returns(currentUser);
+
+        var restaurants = new List<Restaurant>()
+            {
+                new()
+                {
+                    OwnerId = currentUser.Id,
+                },
+                new()
+                {
+                    OwnerId = "2",
+                },
+            };
+
+        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
+        restaurantsRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
+
+        var requirement = new CreatedMultipleRestaurantsRequirement(2);
+        var handler = new CreatedMultipleRestaurantsRequirementHandler(restaurantsRepositoryMock.Object,
+            userContextMock.Object);
+        var context = new AuthorizationHandlerContext([requirement], null, null);
+
+        // act
+
+        await handler.HandleAsync(context);
+
+        // assert
+
+        context.HasSucceeded.ShouldBeFalse();
+        context.HasFailed.ShouldBeTrue();
+
+    }
+}
