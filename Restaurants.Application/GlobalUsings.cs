456f5a1569866b5cdd47b231dd7361d8af74f3e5
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-09 12:08:49 +1100

    Added seq docker image

--- Restaurants.Application/GlobalUsings.cs
+++ Restaurants.Application/GlobalUsings.cs
@@ -1,21 +1,15 @@
 ﻿global using FluentValidation;
 global using Mapster;
 global using MediatR;
+global using Microsoft.AspNetCore.Identity;
 global using Microsoft.Extensions.Logging;
 
 global using Restaurants.Domain.Repositories;
 global using Restaurants.Domain.CQRS;
 global using Restaurants.Domain.Entities;
 global using Restaurants.Domain.Exceptions;
+global using Restaurants.Domain.Identity;
 
 global using Restaurants.Application.Restaurants.Dtos;
 global using Restaurants.Application.Restaurants.Commands;
-global using Restaurants.Application.Restaurants.Queries;
 global using Restaurants.Application.Dishes.Dtos;
-
-global using Microsoft.AspNetCore.Identity;
-global using Restaurants.Domain.Identity;
-
-//global using Microsoft.AspNetCore.Authorization;
-//global using Restaurants.Application.Restaurants.Dtos;
-//global using Restaurants.Domain.Identity;
