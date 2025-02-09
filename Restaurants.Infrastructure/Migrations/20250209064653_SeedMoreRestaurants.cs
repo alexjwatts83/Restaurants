9a245f69ef8ee15fb9950d3c266c0ab25da50a2c
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-09 18:05:41 +1100

    Added seed migration via SQL

---
+++ Restaurants.Infrastructure/Migrations/20250209064653_SeedMoreRestaurants.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMoreRestaurants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var dirPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            
+            var path = dirPath + @$"/Seeders/INSERT_Restaurants.sql";
+
+            var exists = File.Exists(path);
+
+            if (!exists)
+                throw new InvalidOperationException($"SQL file doesn't exists, {path}");
+
+            var sql = File.ReadAllText(path);
+
+            if (sql == null)
+                throw new InvalidOperationException($"SQL file doesnt have any SQL");
+
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
