0aba59c3ebdafb0595755a4d5b3094f714d132ab
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-09 11:17:40 +1100

    Added identity controller

---
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Restaurants.Application.Users.Commands;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/identity")]
+public class IdentityController(IMediator mediator) : ControllerBase
+{
+    [HttpPatch("user")]
+    [Authorize]
+    public async Task<IActionResult> UpdateUserDetails(UpdateUserDetailsCommand command)
+    {
+        await mediator.Send(command);
+
+        return NoContent();
+    }
+}
---
+++ Restaurants.Application/Users/Commands/UpdateUserDetails.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Restaurants.Domain.Identity;
+
+namespace Restaurants.Application.Users.Commands;
+
+public class UpdateUserDetailsCommand : ICommand
+{
+    public DateOnly? DateOfBirth { get; set; }
+    public string? Nationality { get; set; }
+}
+
+public class UpdateUserDetailsCommandHandler(IUserContext userContext,
+    IUserStore<AppUser> userStore,
+    ILogger<UpdateUserDetailsCommandHandler> logger)
+    : ICommandHandler<UpdateUserDetailsCommand>
+{
+    public async Task<Unit> Handle(UpdateUserDetailsCommand request, CancellationToken cancellationToken)
+    {
+        var user = userContext.GetCurrentUser();
+
+        logger.LogInformation("Updating user: {UserId}, with {@Request}", user!.Id, request);
+
+        var dbUser = await userStore.FindByIdAsync(user!.Id, cancellationToken);
+
+        if (dbUser == null)
+            throw new NotFoundException(nameof(AppUser), user!.Id);
+
+        dbUser.Nationality = request.Nationality;
+        dbUser.DateOfBirth = request.DateOfBirth;
+
+        await userStore.UpdateAsync(dbUser, cancellationToken);
+
+        return Unit.Value;
+    }
+}
